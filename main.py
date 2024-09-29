e197985cbefc13a3bf8e370ec1346cfe495876f3
Kumeanie <kumean13@gmail.com>
2024-09-29 21:21:06 +0200

    enregistrement importation carte dans le main

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import discord
 from discord.ext import commands
 import os
 import webserver
+import carte
 
 DISCORD_TOKEN = os.environ['discordkey']
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-
+bot.load_extension("carte")
 
 webserver.keep_alive()
 bot.run(DISCORD_TOKEN)
