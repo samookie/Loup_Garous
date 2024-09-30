ca780f696d104ab99e7ffcd188b5f6ca5ad69725
shenda <chhim.shenda@gmail.com>
2024-09-30 13:40:06 +0200

    test push main.py du code carte.py

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ DISCORD_TOKEN = os.environ['discordkey']
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-bot.load_extension("carte")
-
+#bot.load_extension("carte")
+@commands.command()
+async def carte(ctx):
+    embed = discord.Embed(
+        color=discord.Colour.red().value(),
+        description="Le loup garou peut manger un caca chaque nuit",
+        title="Tu es le Loup Garou"
+    )
+    await ctx.send(embed=embed)
 webserver.keep_alive()
 bot.run(DISCORD_TOKEN)
