d08f67f0563d7ea757d2b884b8bd8e9e62213d27
Kumeanie <Kumean13@gmail.com>
2024-09-30 13:33:25 +0200

    update carte.py

--- carte.py
+++ carte.py
@@ -1,18 +1,13 @@
 from discord.ext import commands
 #hfihif
 @commands.command()
-async def hello(ctx):
-<<<<<<< Updated upstream
-    await ctx.send('Hello {0.display_name}.'.format(ctx.author))
-    
-=======
-    embed = discord.embed(
-        colour=discord.Colour.blue()
-        description="Le loup garou peut éliminé un joueur au choix chaque nuit",
+async def carte(ctx):
+    embed = discord.Embed(
+        colour=discord.Colour.red()
+        description="Le loup garou peut manger un caca chaque nuit",
         title="Tu es le Loup Garou"
     )
     await ctx.send(embed=embed)
->>>>>>> Stashed changes
 
 async def setup(bot):
-    bot.add_command(hello)
+    bot.add_command(carte)
