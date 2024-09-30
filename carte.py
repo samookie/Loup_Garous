8f1c98501574d32154fe70d7423320ed83d05357
shenda <chhim.shenda@gmail.com>
2024-09-30 13:12:55 +0200

    test commit

--- carte.py
+++ carte.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
-
+#hfihif
 @commands.command()
 async def hello(ctx):
     await ctx.send('Hello {0.display_name}.'.format(ctx.author))
+    
 
 async def setup(bot):
     bot.add_command(hello)
